c9339784cf13bc70b514eb7b1a9a66863a972428
Pathnh867 <117632663+Pathnh867@users.noreply.github.com>
2025-07-04 16:27:20 +0700

    AddProductActivity

--- app/src/main/java/com/vietflight/inventory/VietFlightApplication.java
+++ app/src/main/java/com/vietflight/inventory/VietFlightApplication.java
@@ -2,12 +2,23 @@ package com.vietflight.inventory;
 
 import android.app.Application;
 import com.google.firebase.FirebaseApp;
-
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory;
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory;
+import com.vietflight.inventory.BuildConfig;
 public class VietFlightApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         // Khởi tạo Firebase cho toàn bộ app
         FirebaseApp.initializeApp(this);
+        FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
+        if (BuildConfig.DEBUG) {
+            firebaseAppCheck.installAppCheckProviderFactory(
+                    DebugAppCheckProviderFactory.getInstance());
+        } else {
+            firebaseAppCheck.installAppCheckProviderFactory(
+                    PlayIntegrityAppCheckProviderFactory.getInstance());
+        }
     }
 }
